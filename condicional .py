7daea3875838e59410a834866e8c10f64c07e9db
dipperx03 <crisfernando0311@gmail.com>
2025-03-21 20:27:59 -0300

    Create condicional .py

---
+++ condicional .py
@@ -0,0 +1,10 @@
+idade = int(input("Digite sua idade"))
+
+if idade >= 18:
+  print("Voce é maior de idade.")
+
+elif idade >= 16:
+  print ("Voce pode votar mais nao dirgir")
+
+else:
+  print("Voce é menor de idade")
